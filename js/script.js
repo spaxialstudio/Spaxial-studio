db59d8824e3b64a1d66554add450d44b6c2c078a
spaxialstudio <readwithme934@gmail.com>
2025-02-17 01:12:37 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,56 +1,19 @@
-// Obtén los botones de navegación
-const blogBtn = document.getElementById("blog-btn");
-const learnBtn = document.getElementById("learn-btn");
-const portfolioBtn = document.getElementById("portfolio-btn");
-const shopBtn = document.getElementById("shop-btn");
-const whoIsBtn = document.getElementById("whois-btn");
-const gustosBtn = document.getElementById("gustos-btn");
-
-// Obtén las secciones
-const blogSection = document.getElementById("blog");
-const learnSection = document.getElementById("learn");
-const portfolioSection = document.getElementById("portfolio");
-const shopSection = document.getElementById("shop");
-const whoIsSection = document.getElementById("whois");
-const gustosSection = document.getElementById("gustos");
+// Obtén todos los botones de navegación
+const navButtons = document.querySelectorAll(".nav-btn");
 
 // Función para ocultar todas las secciones
 function hideSections() {
-    blogSection.classList.remove("active");
-    learnSection.classList.remove("active");
-    portfolioSection.classList.remove("active");
-    shopSection.classList.remove("active");
-    whoIsSection.classList.remove("active");
-    gustosSection.classList.remove("active");
+    const sections = document.querySelectorAll(".section");
+    sections.forEach(section => {
+        section.classList.remove("active");
+    });
 }
 
 // Asigna los eventos de clic a los botones
-blogBtn.addEventListener("click", function() {
-    hideSections();
-    blogSection.classList.add("active");
-});
-
-learnBtn.addEventListener("click", function() {
-    hideSections();
-    learnSection.classList.add("active");
-});
-
-portfolioBtn.addEventListener("click", function() {
-    hideSections();
-    portfolioSection.classList.add("active");
-});
-
-shopBtn.addEventListener("click", function() {
-    hideSections();
-    shopSection.classList.add("active");
-});
-
-whoIsBtn.addEventListener("click", function() {
-    hideSections();
-    whoIsSection.classList.add("active");
-});
-
-gustosBtn.addEventListener("click", function() {
-    hideSections();
-    gustosSection.classList.add("active");
+navButtons.forEach(button => {
+    button.addEventListener("click", function() {
+        const target = this.getAttribute("data-target"); // Obtén el valor de data-target
+        hideSections(); // Oculta todas las secciones
+        document.getElementById(target).classList.add("active"); // Muestra la sección correspondiente
+    });
 });
