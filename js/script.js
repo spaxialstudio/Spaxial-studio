2a71778674f501c84b90f68b0767c3fe9a8ee672
spaxialstudio <readwithme934@gmail.com>
2025-02-17 01:17:27 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,13 +5,14 @@ const navButtons = document.querySelectorAll(".nav-btn");
 function hideSections() {
     const sections = document.querySelectorAll(".section");
     sections.forEach(section => {
-        section.classList.remove("active");
+        section.classList.remove("active"); // Elimina la clase active para ocultarlas
     });
 }
 
 // Asigna los eventos de clic a los botones
 navButtons.forEach(button => {
-    button.addEventListener("click", function() {
+    button.addEventListener("click", function(e) {
+        e.preventDefault(); // Evita que el enlace realice una acción predeterminada
         const target = this.getAttribute("data-target"); // Obtén el valor de data-target
         hideSections(); // Oculta todas las secciones
         document.getElementById(target).classList.add("active"); // Muestra la sección correspondiente
